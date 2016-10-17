04edd32de9041f1eed912cac1a7e7ddf081ea172
汤鹏 <taanng@126.com>
2016-10-17 23:45:48 +0800

    modified:   Debug/makefile  	modified:   Debug/objects.mk  	modified:   Debug/subdir.mk

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -4,13 +4,13 @@
 
 # Add inputs and outputs from these tool invocations to the build variables
 CPP_SRCS += \
-../test.cpp 
+../test.cpp
 
 OBJS += \
-./testfilesystem.o
+./test.o
 
 CPP_DEPS += \
-./testfilesystem.d
+./test.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
