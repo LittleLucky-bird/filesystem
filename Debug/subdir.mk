b4c3288dace7db37ea6bff8d480738b102556cf8
汤鹏 <taanng@126.com>
2016-10-17 23:28:32 +0800

    modified:   Debug/subdir.mk

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -2,15 +2,15 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 CPP_SRCS += \
-../testfilesystem.cpp 
+../test.cpp 
 
 OBJS += \
-./testfilesystem.o 
+./testfilesystem.o
 
 CPP_DEPS += \
-./testfilesystem.d 
+./testfilesystem.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
@@ -20,5 +20,3 @@ CPP_DEPS += \
 	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
-
-
